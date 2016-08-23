ccbf909b39fd7c0604b239626789d3f3b43e2c2f
Ben Gyori <ben.gyori@gmail.com>
2016-08-23 11:34:01 -0400

    End-to-end INDRA Statement propagation

--- webapp/indra_web.py
+++ webapp/indra_web.py
@@ -1,5 +1,6 @@
-from indra.trips import trips_api
-from indra.pysb_assembler import PysbAssembler
+from indra import trips
+from indra.assemblers import PysbAssembler
+from indra.statements import Statement
 from flask import Flask, render_template, request
 from wtforms import Form
 from wtforms.fields import StringField, SelectMultipleField
@@ -13,25 +14,26 @@ app = Flask(__name__)
 class TripsForm(Form):
     trips_input = StringField('TRIPS text', widget=TextArea())
     trips_process = SubmitField('>>')
-    statements_list = SelectMultipleField('TRIPS Statements', choices = [])
+    trips_stmts = SelectMultipleField('TRIPS Statements', choices=[])
+    trips_select = SubmitField('>>')
 
 class ReachForm(Form):
     reach_input = TextField('REACH PMID')
     reach_process = SubmitField('>>')
-    statements_list = SelectMultipleField('REACH Statements', choices = [])
+    reach_stmts = SelectMultipleField('REACH Statements', choices = [])
 
 class BiopaxForm(Form):
     biopax_input = TextField('BioPax Genes')
     biopax_process = SubmitField('>>')
-    statements_list = SelectMultipleField('BioPAX Statements', choices = [])
+    biopax_stmts = SelectMultipleField('BioPAX Statements', choices = [])
 
 class BelForm(Form):
     bel_input = TextField('BEL Genes')
     bel_process = SubmitField('>>')
-    statements_list = SelectMultipleField('BEL Statements', choices = [])
+    bel_stmts = SelectMultipleField('BEL Statements', choices = [])
 
 class IndraForm(Form):
-    indra_list = SelectMultipleField('INDRA Statements', choices = [])
+    indra_stmts = SelectMultipleField('INDRA Statements', choices = [])
     remove = SubmitField('Remove')
     select_all = SubmitField('Select all')
     select_none = SubmitField('Select none')
@@ -52,57 +54,59 @@ def get_statements(txt):
     stmts = [Complex([Agent('A'), Agent('B')]), Complex([Agent('C'), Agent('D')])]
     return stmts
 
-    tp = trips_api.process_text(txt)
+    tp = trips.process_text(txt)
     if tp is not None:
         return tp.statements
     else:
         return None
 
-stmts = []
-txt = ''
+def get_stmt_list(stmts):
+    list_choices = []
+    for i, st in enumerate(stmts):
+        choice = ('%d' % i, '%s' % st)
+        list_choices.append(choice)
+    return list_choices
+
+global trips_stmts
+global indra_stmts
+trips_stmts = []
+indra_stmts = []
 
 @app.route("/", methods=['POST', 'GET'])
 def run():
+    global trips_stmts
+    global indra_stmts
+    print trips_stmts
     trips_form = TripsForm(request.form)
     reach_form = ReachForm(request.form)
     biopax_form = BiopaxForm(request.form)
     bel_form = BelForm(request.form)
     indra_form = IndraForm(request.form)
-    stmts = []
     pysb_model = ''
+    print request.form
+    print trips_form.data
     if request.method == 'POST':
         #stmts = form.statements_list.choices
         if request.form.get('trips_process'):
             print 'Trips process'
             txt = request.form['trips_input']
-            stmts = get_statements(txt)
-            stmt_list = [(str(i), str(stmts[i])) for i in range(len(stmts))]
-            trips_form.statements_list.choices = stmt_list
-        elif request.form.get('reach_process'):
+            trips_stmts = get_statements(txt)
+            trips_stmts_list = get_stmt_list(trips_stmts)
+        if request.form.get('trips_select'):
+            indra_stmts = trips_stmts
+        if request.form.get('reach_process'):
             print 'Reach process'
         elif request.form.get('biopax_process'):
             print 'Biopax process'
         elif request.form.get('bel_process'):
             print 'BEL process'
+        elif request.form.get('pysb_assemble'):
+            print indra_stmts
+            pysb_model = get_pysb_model(indra_stmts)
         else:
             print 'Other'
-        '''
-        if button == 'stmt':
-            txt = form.trips_input.data
-            if txt:
-                stmts = get_statements(txt)
-                stmt_list = [(str(i), str(stmts[i])) for i in range(len(stmts))]
-            else:
-                stmts = []
-            form.statements_list.choices = stmt_list
-        elif button == 'pysba':
-            print  form.statements_list.__dict__
-            pysb_model = get_pysb_model(stmts)
-            # pysb_model = 'No input text'
-        else:
-            stmts = None
-            pysb_model = ''
-        '''
+        trips_form.trips_stmts.choices = get_stmt_list(trips_stmts)
+        indra_form.indra_stmts.choices = get_stmt_list(indra_stmts)
     args = {'trips_form': trips_form,
             'reach_form': reach_form,
             'biopax_form': biopax_form,

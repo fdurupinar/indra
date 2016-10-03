c58ab823efbc2db746e31ceefab60c33eefe94a4
Petar Todorov <petar.v.todorov@gmail.com>
2016-10-03 17:12:16 -0400

    First implementation of the slack bot. Go to channel "bot_testing" and type in "indrabot ....". The bot should repeat back what you say.

--- indra/tools/indra_slack_bot.py
+++ indra/tools/indra_slack_bot.py
@@ -1,1 +1,37 @@
-import indra
+from slackclient import SlackClient
+
+token = open('indrabot_slack_token', 'r').read().strip()     # found at https://api.slack.com/web#authentication
+sc = SlackClient(token)import
+
+
+import time
+import json
+
+if sc.rtm_connect():
+    while True:
+
+        user = ''
+        msg = ''
+        reply = ''
+
+        time.sleep(1)
+        a = sc.rtm_read()
+
+        if len(a)>0:
+            try:
+                event_type = a[0]['type']
+                if event_type == 'message':
+                    msg = a[0]['text']
+                    user = a[0]['user']
+                    channel = a[0]['channel']
+
+                    user_info = json.loads(sc.server.api_call("users.info",users =user))
+                    username = user_info['users'][0]['name']
+
+                    if msg[0:8] == 'indrabot':
+                        reply = username + ' just said: ' + '"' + msg + '"'
+                        sc.rtm_send_message(channel, reply)
+
+
+            except:
+                None

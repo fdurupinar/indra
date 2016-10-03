47e216a91f306ac5a87fceb9b6943c064d7cb7e8
Ben Gyori <ben.gyori@gmail.com>
2016-10-03 18:58:19 -0400

    Refactoring, logging and handle interrupts

--- indra/tools/indra_slack_bot.py
+++ indra/tools/indra_slack_bot.py
@@ -1,10 +1,14 @@
 import sys
 import time
 import json
+import indra
 import logging
 from slackclient import SlackClient
 
 logger = logging.getLogger('indra_slack_bot')
+
+user_cache = {}
+channel_cache = {}
 
 def read_slack_token(fname=None):
     # Token can be found at https://api.slack.com/web#authentication
@@ -18,24 +22,59 @@ def read_slack_token(fname=None):
         logger.error('Could not read Slack token from %s.' % fname)
         return None
 
+def get_user_name(sc, user_id):
+    user_name = user_cache.get(user_id)
+    if user_name:
+        return user_name
+    res = sc.server.api_call('users.info', users=user_id)
+    user_info = json.loads(res)
+    for user in user_info['users']:
+        if user['id'] == user_id:
+            user_cache[user_id] = user['name']
+            return user['name']
+    return None
+
+def get_channel_name(sc, channel_id):
+    channel_name = channel_cache.get(channel_id)
+    if channel_name:
+        return channel_name
+    res = sc.server.api_call('channels.info', channel=channel_id)
+    channel_info = json.loads(res)
+    channel = channel_info['channel']
+    if channel['id'] == channel_id:
+        channel_cache[channel_id] = channel['name']
+        return channel['name']
+    return None
+
 def read_message(sc):
-    event = sc.rtm_read()
-    if event:
-        try:
-            event_type = event[0]['type']
-            if event_type == 'message':
-                msg = event[0]['text']
-                user = event[0]['user']
-                channel = event[0]['channel']
-                user_info = json.loads(sc.server.api_call("users.info", users=user))
-                username = user_info['users'][0]['name']
-                return (channel, username, msg)
-        except:
-            pass
+    events = sc.rtm_read()
+    logger.info('%s events happened' % len(events))
+    if not events:
+        return None
+    try:
+        event = events[0]
+        event_type = event.get('type')
+        if not event_type:
+            return
+        if event_type == 'message':
+            msg = event['text']
+            user = event['user']
+            channel = event['channel']
+            user_name = get_user_name(sc, user)
+            channel_name = get_channel_name(sc, channel)
+            logger.info('Message received - [%s/%s]: %s' %
+                        (channel_name, user_name, msg))
+            return (channel, user_name, msg)
+    except Exception as e:
+        logger.error('Could not read message')
+        logger.error(e)
     return None
 
 def send_message(sc, channel, msg):
     sc.rtm_send_message(channel, msg)
+    channel_name = get_channel_name(sc, channel)
+    logger.info('Message sent - [%s]: %s' %
+                (channel_name, msg))
 
 if __name__ == '__main__':
     token = read_slack_token()
@@ -47,10 +86,14 @@ if __name__ == '__main__':
         logger.error('Could not connect to Slack.')
         sys.exit()
     while True:
-        time.sleep(1)
-        res = read_message(sc)
-        if res:
-            (channel, username, msg) = res
-            if msg[0:8] == 'indrabot':
-                reply = username + ' just said: ' + '"' + msg + '"'
-                send_message(sc, channel, reply)
+        try:
+            time.sleep(1)
+            res = read_message(sc)
+            if res:
+                (channel, username, msg) = res
+                if msg[0:8] == 'indrabot':
+                    reply = username + ' just said: ' + '"' + msg + '"'
+                    send_message(sc, channel, reply)
+        except KeyboardInterrupt:
+            logger.info('Shutting down due to keyboard interrupt.')
+            sys.exit()

11af42ae011f4ae46f44f10ffd87884ef35cccb8
Ben Gyori <ben.gyori@gmail.com>
2016-10-03 17:41:31 -0400

    Refactoring slack bot code

--- indra/tools/indra_slack_bot.py
+++ indra/tools/indra_slack_bot.py
@@ -1,37 +1,56 @@
-from slackclient import SlackClient
-
-token = open('indrabot_slack_token', 'r').read().strip()     # found at https://api.slack.com/web#authentication
-sc = SlackClient(token)import
-
-
+import sys
 import time
 import json
+import logging
+from slackclient import SlackClient
 
-if sc.rtm_connect():
+logger = logging.getLogger('indra_slack_bot')
+
+def read_slack_token(fname=None):
+    # Token can be found at https://api.slack.com/web#authentication
+    if fname is None:
+        fname = 'indrabot_slack_token'
+    try:
+        with open(fname, 'rt') as fh:
+            token = fh.read().strip()
+        return token
+    except IOError:
+        logger.error('Could not read Slack token from %s.' % fname)
+        return None
+
+def read_message(sc):
+    event = sc.rtm_read()
+    if event:
+        try:
+            event_type = event[0]['type']
+            if event_type == 'message':
+                msg = event[0]['text']
+                user = event[0]['user']
+                channel = event[0]['channel']
+                user_info = json.loads(sc.server.api_call("users.info", users=user))
+                username = user_info['users'][0]['name']
+                return (channel, username, msg)
+        except:
+            pass
+    return None
+
+def send_message(sc, channel, msg):
+    sc.rtm_send_message(channel, msg)
+
+if __name__ == '__main__':
+    token = read_slack_token()
+    if not token:
+        sys.exit()
+    sc = SlackClient(token)
+    conn = sc.rtm_connect()
+    if not conn:
+        logger.error('Could not connect to Slack.')
+        sys.exit()
     while True:
-
-        user = ''
-        msg = ''
-        reply = ''
-
         time.sleep(1)
-        a = sc.rtm_read()
-
-        if len(a)>0:
-            try:
-                event_type = a[0]['type']
-                if event_type == 'message':
-                    msg = a[0]['text']
-                    user = a[0]['user']
-                    channel = a[0]['channel']
-
-                    user_info = json.loads(sc.server.api_call("users.info",users =user))
-                    username = user_info['users'][0]['name']
-
-                    if msg[0:8] == 'indrabot':
-                        reply = username + ' just said: ' + '"' + msg + '"'
-                        sc.rtm_send_message(channel, reply)
-
-
-            except:
-                None
+        res = read_message(sc)
+        if res:
+            (channel, username, msg) = res
+            if msg[0:8] == 'indrabot':
+                reply = username + ' just said: ' + '"' + msg + '"'
+                send_message(sc, channel, reply)

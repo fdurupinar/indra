aaddaa56aef7d24b1e7815be56f038b4a9faa8c7
Ben Gyori <ben.gyori@gmail.com>
2017-01-31 13:03:36 -0500

    Initial processing IntAct kinetic rates

---
+++ indra/databases/intact_client.py
@@ -0,0 +1,42 @@
+import re
+import json
+from sympy.physics import units
+
+M = units.moles / units.liter
+
+def parse_value(value):
+    # Pattern 1 example: 31.0x10^-6(molar)
+    pattern = r'([0-9]+\.[0-9]+)x10\^([-]?[^ ]+)\(molar\)'
+    res = re.match(pattern, value)
+    if res:
+        num, exponent = res.groups()
+        rate_constant = float(num) * 10**float(exponent) * M
+        return rate_constant
+
+    # Pattern 2 example: 1.5x10^-6 ~0.5(molar)
+    # Pattern 3 example: 23.2x10^-6(mol)
+    # Pattern 4 example: 0.76(second -1)
+    # Pattern 5 example: 1.9x10^6 ~0.03(per mole per second)
+
+    print('Could not parse %s' % value)
+    return None
+
+def parse_entry(entry):
+    parts = entry.split('|')
+    rates = {}
+    for part in parts:
+        prefix, value = part.split(':')
+        rate_constant = parse_value(value)
+        if rate_constant is not None:
+            rates[prefix] = rate_constant
+    return rates
+
+
+if __name__ == '__main__':
+    with open('parameters.json', 'r') as fh:
+        all_entries = json.load(fh)
+    prefixes = set()
+    for entry in all_entries:
+        rates = parse_entry(entry)
+        if rates:
+            print(rates)
